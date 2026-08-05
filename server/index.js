5bf822e89eac520e4440d5fc4143ed539710088c
chandan Pragapati <chandan22781310@gmail.com>
2026-08-05 21:37:50 +0530

    Fix CORS for Vercel

--- server/index.js
+++ server/index.js
@@ -22,12 +22,21 @@ const app = express();
 // Middleware
 // =====================
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://localhost:5174",
+  "https://eventora-pink.vercel.app"
+];
+
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-
-    ],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
